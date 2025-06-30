8bc4b17782cf12dbcc91308a4d8c7fc82e073824
khanz0613 <khanz0613@naver.com>
2025-06-30 16:11:33 +0900

    🐛 FIX : Tour 관련 수정

--- Back-end/src/main/java/com/example/backend/tour/entity/Tour.java
+++ Back-end/src/main/java/com/example/backend/tour/entity/Tour.java
@@ -3,9 +3,7 @@ package com.example.backend.tour.entity;
 import com.example.backend.cart.entity.Cart;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
@@ -21,23 +19,29 @@ public class Tour {
     @Column(name = "tour_id", columnDefinition = "BINARY(16)", nullable = false)
     private UUID tourId;
 
-    @Column(name = "longitude", precision = 10, scale = 7, nullable = false)
-    private BigDecimal longitude;
+    @Column(name = "longitude")
+    private Double longitude;
 
-    @Column(name = "latitude", precision = 10, scale = 7, nullable = false)
-    private BigDecimal latitude;
+    @Column(name = "latitude")
+    private Double latitude;
 
-    @Column(name = "address", length = 255, nullable = false)
+    @Column(name = "address", length = 500)
     private String address;
 
-    @Column(name = "image", length = 255)
+    @Column(name = "image", length = 500)
     private String image;
 
-    @Column(name = "tema", length = 50)
+    @Column(name = "tema", length = 100)
     private String tema;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cart_id", referencedColumnName = "cart_id")
-    private Cart cartId;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private TourCategory category;
 
+    @Column(name = "price")
+    private Long price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cartId;
 }
