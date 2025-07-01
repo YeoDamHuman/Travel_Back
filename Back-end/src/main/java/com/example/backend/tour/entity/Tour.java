a610db375b2b62f12fef3a6171bb4964c3bd79f6
khanz0613 <khanz0613@naver.com>
2025-07-01 17:42:49 +0900

    🐛 FIX : 유니크 인덱스 추가

--- Back-end/src/main/java/com/example/backend/tour/entity/Tour.java
+++ Back-end/src/main/java/com/example/backend/tour/entity/Tour.java
@@ -9,7 +9,8 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tour")
+@Table(name = "tour",
+       uniqueConstraints = @UniqueConstraint(name = "uk_cart_content", columnNames = {"cart_id", "content_id"}))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
