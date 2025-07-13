b7a22a63487b01dd91f16d5efadbdf977e27ecf6
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-07-13 18:00:17 +0900

    ✨ FEAT : 어드민 기능 개발

---
+++ Back-end/src/main/java/com/example/backend/admin/dto/request/AdminRequest.java
@@ -0,0 +1,4 @@
+package com.example.backend.admin.dto.request;
+
+public class AdminRequest {
+}
--- Back-end/src/main/java/com/example/backend/comment/repository/CommentRepository.java
+++ Back-end/src/main/java/com/example/backend/comment/repository/CommentRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
     Page<Comment> findByBoardId(Board board, Pageable pageable);
 
+    List<Comment> findByCommentReportGreaterThanEqual(int i);
 }
