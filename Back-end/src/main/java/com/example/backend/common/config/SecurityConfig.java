eb3f58fc4d2039e60e481bd593d5b6e10a81cda3
khanz0613 <62611268+khanz0613@users.noreply.github.com>
2025-09-24 17:20:04 +0900

    [feat] 앱 CORS 해결 위한 경로 추가

--- Back-end/src/main/java/com/example/backend/common/config/SecurityConfig.java
+++ Back-end/src/main/java/com/example/backend/common/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "http://yeodam.site", "https://yeodam.site", "https://yeodam.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "http://yeodam.site", "https://yeodam.site", "https://yeodam.vercel.app", "https://localhost"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
